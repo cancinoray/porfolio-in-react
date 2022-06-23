fb895f6469ac4fb47c3701085c43e4f0365568e0
Raymond <cancinoray@gmail.com>
2022-06-23 21:30:41 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header'
 import logo from './assets/logo.svg'
+import Profile from './pages/Profile'
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo}/>
+      <Profile userName='cancinoray' />
     </div>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,46 @@
-function Profile(){
+import { useState, useEffect } from 'react';
+
+
+function Profile({ userName }){
+	const [loading, setLoading] = useState(false);
+	const [profile, setProfile] = useState({});
+
+	useEffect(() => {
+		async function fetchData(){
+			const profile = await fetch(`https://api.github.com/users/${userName}`);
+			const result = await profile.json();
+
+			if(result){
+				setProfile(result);
+				setLoading(false);
+			}
+		}
+		fetchData();
+	}, [userName]);
+
+
+
 	return(
+		<div>
+		<h2>About Me</h2>
+		{loading
+			? ( <span>Loading. . . </span> )	
+			: ( <url>
+					<li><span>avatar_url:</span>{profile.avatar_url}</li>
+					<li><span>html_url:</span>{profile.html_url}</li>
+					<li><span>repos_url:</span>{profile.repos_url}</li>
+					<li><span>name:</span>{profile.name}</li>
+					<li><span>company:</span>{profile.company}</li>
+					<li><span>location:</span>{profile.location}</li>
+					<li><span>email:</span>{profile.email}</li>
+					<li><span>bio:</span>{profile.bio}</li>
+					{console.log(profile.login)}
+				</url>
+				)
+		}
 
-
+		</div>	
+		
 	);
 }
 
